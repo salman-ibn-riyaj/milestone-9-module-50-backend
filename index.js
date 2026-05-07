d0fbc9bb0486af63e563974216664804f9fe0dd8
salman <salmanibnriyaj@gmail.com>
2026-05-07 18:02:32 +0600

    username and password hide

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://simpleCrudServer:xfWKc0lEvAkmkebu@cluster0.uqn5hx4.mongodb.net/?appName=Cluster0";
+const uri = process.env.MONGODB_URI
+  ;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
